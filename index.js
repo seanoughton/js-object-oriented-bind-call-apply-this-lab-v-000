da4c7958046d47ca913081258f00a7c9fcc399a0
Sean Oughton <seanoughton@gmail.com>
2018-06-28 20:00:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 //justInvoke(fn): The function simply invokes the function passed through to it. It also returns the return value of the passed through function.
 
 function justInvoke(fn) {
-
+  fn();
 };
