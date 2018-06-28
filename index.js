9643891266dbfb4a1311bffbb23cd3a6579d1752
Sean Oughton <seanoughton@gmail.com>
2018-06-28 20:05:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,7 @@ function setThisWithApply(fn, thisValue, args) {
 };
 
 //returnNewFunctionOf(functionToBeCopied, thisValue): Here, we ask you to write a function that returns a copy of the function passed through, but sets the this value of the function's copy.
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  
+};
